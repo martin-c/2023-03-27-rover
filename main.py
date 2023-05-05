e83f77e22836ceaa9bf37cceb6530f7f23b4dcd5
martin-c <martin.clemons@mac.com>
2023-05-06 01:51:31 +0200

    Add arrows on LED screen, update sounds, add minimum obstacle receive power

--- main.py
+++ main.py
@@ -1,38 +1,65 @@
+def power(base: number, exponent: number):
+    return Math.exp(exponent * Math.log(base))
+
 def on_button_pressed_ab():
     input.calibrate_compass()
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 def on_received_string(receivedString):
-    if receivedString == "ping":
+    if receivedString == "ping" and radio.received_packet(RadioPacketProperty.SIGNAL_STRENGTH) > -40:
+        radio.send_string("png")
         basic.show_icon(IconNames.CONFUSED)
         music.play_sound_effect(music.builtin_sound_effect(soundExpression.sad),
             SoundExpressionPlayMode.UNTIL_DONE)
     elif receivedString == "fwd":
-        images.arrow_image(ArrowNames.NORTH).show_image(0)
-        music.play_sound_effect(music.builtin_sound_effect(soundExpression.giggle),
+        music.play_sound_effect(music.create_sound_effect(WaveShape.SINE,
+                776,
+                3261,
+                255,
+                255,
+                400,
+                SoundExpressionEffect.VIBRATO,
+                InterpolationCurve.CURVE),
             SoundExpressionPlayMode.UNTIL_DONE)
+        images.arrow_image(ArrowNames.NORTH).show_image(0)
 radio.on_received_string(on_received_string)
 
 def on_received_value(name, value):
     global hdg_delta
-    hdg_delta = value - input.compass_heading()
+    hdg_delta = value - input.compass_heading() - 15
     if hdg_delta > 180:
         hdg_delta += -360
     elif hdg_delta < -180:
         hdg_delta += 360
-    serial.write_value("hdg_delta", hdg_delta)
-    serial.write_value("pow", 2 ** (hdg_delta / 180))
     if abs(hdg_delta) > 15:
-        music.play_tone(523 * 2 ** (hdg_delta / 180),
+        music.play_tone(523 * power(3, hdg_delta / 180),
             music.beat(BeatFraction.EIGHTH))
 radio.on_received_value(on_received_value)
 
 hdg_delta = 0
 radio.set_group(1)
-music.set_volume(200)
+music.set_volume(250)
 hdg_delta = 0
 basic.show_icon(IconNames.YES)
 
 def on_forever():
-    basic.show_icon(IconNames.HAPPY)
+    if abs(hdg_delta) > 15:
+        if hdg_delta > 0:
+            basic.show_leds("""
+                    . . # . .
+                                                    . . . # .
+                                                    . # # # #
+                                                    # . . # .
+                                                    # . # . .
+                """)
+        else:
+            basic.show_leds("""
+                    . . # . .
+                                                    . # . . .
+                                                    # # # # .
+                                                    . # . . #
+                                                    . . # . #
+                """)
+    else:
+        basic.show_icon(IconNames.HAPPY)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -5,24 +5,24 @@ input.onButtonPressed(Button.AB, function () {
     input.calibrateCompass()
 })
 radio.onReceivedString(function (receivedString) {
-    if (receivedString == "ping") {
+    if (receivedString == "ping" && radio.receivedPacket(RadioPacketProperty.SignalStrength) > -85) {
         radio.sendString("png")
         basic.showIcon(IconNames.Confused)
         music.playSoundEffect(music.builtinSoundEffect(soundExpression.sad), SoundExpressionPlayMode.UntilDone)
     } else if (receivedString == "fwd") {
-        images.arrowImage(ArrowNames.North).showImage(0)
         music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 776, 3261, 255, 255, 400, SoundExpressionEffect.Vibrato, InterpolationCurve.Curve), SoundExpressionPlayMode.UntilDone)
+        images.arrowImage(ArrowNames.North).showImage(0)
     }
 })
 radio.onReceivedValue(function (name, value) {
-    hdg_delta = value - input.compassHeading()
+    hdg_delta = value - input.compassHeading() - 15
     if (hdg_delta > 180) {
         hdg_delta += -360
     } else if (hdg_delta < -180) {
         hdg_delta += 360
     }
     if (Math.abs(hdg_delta) > 15) {
-        music.playTone(523 * power(2, hdg_delta / 180), music.beat(BeatFraction.Eighth))
+        music.playTone(523 * power(3, hdg_delta / 180), music.beat(BeatFraction.Eighth))
     }
 })
 let hdg_delta = 0
@@ -31,5 +31,25 @@ music.setVolume(250)
 hdg_delta = 0
 basic.showIcon(IconNames.Yes)
 basic.forever(function () {
-    basic.showIcon(IconNames.Happy)
+    if (Math.abs(hdg_delta) > 15) {
+        if (hdg_delta > 0) {
+            basic.showLeds(`
+                . . # . .
+                . . . # .
+                . # # # #
+                # . . # .
+                # . # . .
+                `)
+        } else {
+            basic.showLeds(`
+                . . # . .
+                . # . . .
+                # # # # .
+                . # . . #
+                . . # . #
+                `)
+        }
+    } else {
+        basic.showIcon(IconNames.Happy)
+    }
 })
